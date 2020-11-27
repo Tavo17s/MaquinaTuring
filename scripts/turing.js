cfdfb7c8bd04da6feb225509fd5d49c5f0d5516a
Gustavo Solano <tavoskt17@gmail.com>
2020-11-27 15:29:49 -0500

    Version 1.0

---
+++ scripts/turing.js
@@ -0,0 +1,126 @@
+$(function() {
+
+    $('#cargar').click(function(){
+        id = ID;
+        if($('#cadena').val() == ""){
+            $(this).popover('show');
+            return false;
+        }else{
+            $(this).popover('hide');
+            ClearRows(4);
+            LoadString(id);
+            Clear();
+            return true;
+        }
+    });
+    $('#evaluar').click(
+
+        function(){
+            id = ID;
+            time = 1000 - $("#rango").val();
+            Tick = setInterval(function(){Evaluate(id)}, time);
+        }
+
+    );
+});
+
+function AddBlank(table){
+    for(i = 0; i < 100; i++){
+        AddRow(BLANK, "", "", table);
+    }
+    return true;
+}
+
+function LoadString(table){
+    var cadena = $('#cadena').val();
+        ClearRows(table);
+        AddRow('ß', "active-row", "bg-info text-light", table);
+        for(i = 0; i < cadena.length; i++){
+            AddRow(cadena.charAt(i), "", "", table);
+        }
+        AddBlank(table);
+        return true;
+}
+
+function EvaluateStep(symbol, id){
+    Machines[id].Count++;
+    symbols = Machines[id][Machines[id].State]
+    console.log(symbols[symbol]);
+    if(symbols[symbol] == undefined) return { Error : true, Acceptable : Machines[id].Functions.Acceptable(Machines[id].State), Message : `El símbolo <strong>'${symbol}'</strong> no tiene transición definida en el estado <strong>${Machines[id].State}</strong> de esta máquina. ${symbols.ERROR ? symbols.ERROR : ''}`};
+    NextValues = symbols[symbol];
+    Machines[id].State = NextValues[1];
+    return { Error : false, Acceptable: Machines[id].Functions.Acceptable(Machines[id].State), Output : NextValues[0], Movement : NextValues[2] };
+}
+
+function Evaluate(id){
+        chain = $("#tabla" + id + " td");
+        i = Machines[id].i;
+        if(!Machines[id].Functions.Acceptable(Machines[id].State) && chain.length > i && Machines[id].Count < 10000){
+            time = 1000 - $("#rango").val();
+            result = EvaluateStep(chain[i].textContent.trim(), id);
+            if(result.Error){
+                Update();
+                Stop();
+                $('#modalTitle').html('¡Error!');
+                $('#modalText').html(result.Message);
+                $('#myModal').modal('show');
+               
+            }else{
+                if (i >= chain.length - 2) AddBlank(id);
+                chain[i].textContent = (result.Output);
+                i += result.Movement;
+                Machines[id].i += result.Movement;
+                chain[i].setAttribute('id', 'new-row');
+                ChangeActiveRow(id, time);
+                Update();
+            }
+
+            
+        }else{
+            if(Machines[id].Functions.Acceptable(Machines[id].State)){
+                Update();            
+                Stop();   
+            }
+            if(Machines[id].Count >= 10000){
+                Update();
+                Stop();
+                $('#modalTitle').html('¡Error!');
+                $('#modalText').html('La cadena ingresada ha generado muchas transiciones sin definir un resultado.');
+                $('#myModal').modal('show');
+            }
+        }
+
+        
+}
+function Stop(){
+    clearInterval(Tick);
+}
+// SYNTAXIS Machine[CURRENT_STATE] = { INCOMING_SYMBOL: [OUTPUT_SYMBOL, NEXT_STATE, HEAD_MOVEMENT]}
+function SetMachine4(){
+
+    Machines[4][0] = {
+        'a' : ['a', 1, 1],
+        'b' : ['a', 1, 1],
+        '' : ['a', 1, 1],
+        'ß' : ['ß', 1 , 1]
+    }
+    Machines[4][1] = {
+        'a' : ['a', 1, 1],
+        'b' : ['a', 1, 1],
+        '' : ['a', 1, 1],
+        'ß' : ['ß', 2, -1] 
+    }
+    Machines[4][2] = {
+        'a' : ['a', 2, -1],
+        'ß' : ['ß', 3, 1]
+    }
+    Machines[4][3] = {
+        'a' : ['a', 10, 0]
+    }
+    Machines[4]['i'] = 0;
+    Machines[4]['Count'] = 0;
+    Machines[4]['State'] = 0;
+    Machines[4]['Functions'] = {
+        Acceptable(state) { return state == 3 }
+    };
+}
---
+++ scripts/ui.js
@@ -0,0 +1,87 @@
+  /*****************/
+ /* APP VARIABLES */
+/*****************/
+
+const BLANK = 'ß';
+var Tick = undefined;
+ID = 4;
+var Machines = {
+    4 : new Array()
+}
+ClearRows(4);
+
+  /****************/
+ /* UI FUNCTIONS */
+/****************/
+
+$(function() {
+    Clear();
+    $('#info').click(function(){
+        $('#instrucciones' + ID).modal('show');
+    });
+});
+
+function ScrollToID(id, time, table){
+
+    container = $('#tabla' + table);
+    item = $('#' + id);
+    $('body').animate({ scrollTop: item.offset().top - container.offset().top + container.scrollTop() }, time >= 0 ? time:600, 'swing');
+}
+
+function ScrollLeftToID(id, time, table){
+
+    container = $('#tabla' + table);
+    item = $('#' + id);
+    $('#tabla' + table).animate({ scrollLeft: item.offset().left - 5 * container.offset().left + container.scrollLeft() }, time >= 0 ? time:600, 'swing');
+}
+
+function AddRow(value, id_value, class_value, table){
+
+    container = $('#tabla' + table + " tbody")
+    item = `<td id='${id_value}' class='${class_value}'>
+                ${value}
+            </td>`
+    container.append(item);
+    return true
+}
+
+function ClearRows(table){
+
+    $('#tabla' + table + " tbody td").remove();
+    return true
+}
+
+function ChangeActiveRow(table, time) { // This method requieres that 'the caller' sets row's id as 'new-row' before call this. 
+
+    /* Remove old active row*/
+    oldRow = $('#active-row');
+    oldRow.attr('id', '');
+    oldRow.attr('class', '');
+
+    /* Set new active row*/
+    newRow = $('#new-row');
+    newRow.attr('id', 'active-row');    
+    ScrollLeftToID('active-row', time, table);
+    newRow.attr('class', 'bg-info text-light');
+}
+
+function Clear(){
+    // Popups
+    $('[data-toggle="popover1"]').popover();
+    $('#myModal').modal('hide');
+    $('#instrucciones').modal('hide');
+
+    // SET MATCHINE
+    SetMachine4();
+    // SET COUNT
+    $('#contador').text(Machines[ID].Count);
+    // SET STATE TO MACHINE'S HEAD
+    $('#estado').text(Machines[ID].Count);
+}
+
+function Update(){
+       // SET COUNT
+       $('#contador').text(Machines[ID].Count);
+       // SET STATE TO MACHINE'S HEAD
+       $('#estado').text(Machines[ID].State);
+}
